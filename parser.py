ac7a89af7a12ed4eab6baec07258c94837159e40
Senthilnathan Viswanathan <home@DN0a226174.SUNet>
2016-10-16 14:43:58 -0700

    added files

---
+++ parser.py
@@ -0,0 +1,14 @@
+import json
+import pp
+import sys
+
+def main(argv):
+    dataset_dir = argv.pop(1)
+    print dataset_dir
+    with open(dataset_dir+'yelp_academic_dataset_business.json') as f:
+        for line in f:
+            print line
+            break
+
+if __name__ == '__main__':
+	main(sys.argv)
